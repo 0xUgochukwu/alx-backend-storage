62bc70a598d3499f058640990d931ade0b7d443e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-15 13:17:54 +0100

    weighted averages for all

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,20 +5,14 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER %%
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  -- Create a temporary table to store the computed averages
-  CREATE TEMPORARY TABLE tmp_averages AS
-    SELECT U.id, SUM(C.score * P.weight) / SUM(P.weight) AS avg_score
-    FROM users AS U
-    JOIN corrections AS C ON U.id = C.user_id
-    JOIN projects AS P ON C.project_id = P.id
-    GROUP BY U.id;
-
-  -- Update the users table with the computed averages
-  UPDATE users AS U
-  JOIN tmp_averages AS T ON U.id = T.user_id
-  SET U.average_score = T.avg_score;
-
-  -- Drop the temporary table
-  DROP TEMPORARY TABLE IF EXISTS tmp_averages;
+    UPDATE users AS U, 
+        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+        FROM users AS U 
+        JOIN corrections as C ON U.id=C.user_id 
+        JOIN projects AS P ON C.project_id=P.id 
+        GROUP BY U.id)
+    AS WA
+    SET U.average_score = WA.w_avg 
+    WHERE U.id=WA.id;
 END
 %% DELIMITER;
