8e117eef049363bbdc15958b8450972fceaa7357
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-15 11:24:35 +0100

    valid_email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER after_email_update
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-  IF NEW.email <> OLD.email
+  IF NEW.email <> OLD.email THEN
     SET NEW.valid_email = 0;
   END IF;
 END //
