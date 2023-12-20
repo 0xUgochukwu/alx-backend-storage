c8ef9e9a918ea2d7444acd610a2c99731835c23b
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-20 01:49:33 +0100

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,12 +15,12 @@ if __name__ == '__main__':
     n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {method_count}')
 
-    status_check = nginx_collection.count_documents(
+    status_check_count = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
+    print(f'{status_check_count} status check')
