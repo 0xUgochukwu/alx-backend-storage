a2372407c691753ad2d8a919963aeeeadc96e5d7
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-20 02:11:20 +0100

    aggregation

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -12,8 +12,9 @@ def top_students(mongo_collection):
     return mongo_collection.aggregate([
         {
             '$project': {
-            'name': '$name',
-            'averageScore': {'$avg': '$topics.score'}}
+                'name': '$name',
+                'averageScore': {'$avg': '$topics.score'}
+            }
         },
         {'$sort': {'averageScore': -1}}
     ])
